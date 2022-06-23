e56a8f3afdb6a56144b5f6e9ad2d9f9fe97af16c
Nick <docfr10@yandex.ru>
2022-06-23 13:33:14 +0300

    Idle and Move animations are integrated

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,30 +10,41 @@ public class Player : MonoBehaviour
     [SerializeField] private bool is_grounded = false;
 
     private Rigidbody2D rb;
+    private Animator animator;
     private bool is_look_right = true;
 
     private void Awake()
     {
-        //Присваиваем значения Rigidbody игрока к переменным в скрипте
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();//Присваиваем значение Rigidbody игрока к переменным в скрипте
+        animator = GetComponent<Animator>();//Присваиваем значение Animator игрока к переменным в скрипте
+    }
+
+    public enum States //Запись всех состояний анимаций
+    {
+        idle,
+        Move
+    }
+
+    private States State
+    {
+        get { return (States)animator.GetInteger("state"); }//Получаем активное состояние анимации
+        set { animator.SetInteger("state", (int)value); }//Меняем активное состтяние анимации
     }
 
     private void Move()
     {
+        if (is_grounded) State = States.Move;//Как только вызывается метод Move проигрывается анимация перемещения
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
-        //Перемещение игрока
-        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
-        //Поворот игрока налево или направо
-        Flip();
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);//Перемещение игрока
+        Flip();//Поворот игрока налево или направо
     }
 
     private void Jump()
     {
-        //Прыжок игрока
-        rb.AddForce(transform.up * jump_fource, ForceMode2D.Impulse);
+        rb.AddForce(transform.up * jump_fource, ForceMode2D.Impulse);//Прыжок игрока
     }
 
-    private void Flip()
+    private void Flip()//Повороты игрока
     {
         if (Input.GetAxisRaw("Horizontal") == 1)
             transform.localScale = new Vector3(1, 1, 1);
@@ -41,9 +52,8 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
     }
 
-    private void CheckGround()
+    private void CheckGround()//Проверка на то, на земле ли персонаж
     {
-        //Проверка на то, на земле ли персонаж
         Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 1f);
         is_grounded = collider.Length > 1;
     }
@@ -62,6 +72,8 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        if (is_grounded)//Если персонаж на земле, то проигрывается анимация стояния на месте
+            State = States.idle;
         if (Input.GetButton("Horizontal"))
             Move();
         if (is_grounded && Input.GetButton("Jump"))
